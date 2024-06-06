e548f11434b3ecbe44bb57474a4fd3b3d580c157
Algo-E Tech Company <136216514+algoetech@users.noreply.github.com>
2024-06-06 13:44:36 +0300

    gone

--- db/bookstore(2).sql
+++ db/bookstore(2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2024 at 08:36 AM
+-- Generation Time: Jun 06, 2024 at 12:43 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -52,6 +52,7 @@ INSERT INTO `authors` (`id`, `user_id`, `name`, `bio`, `birth_date`, `death_date
 CREATE TABLE `books` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
+  `request_id` bigint(20) UNSIGNED DEFAULT NULL,
   `isbn` varchar(255) NOT NULL,
   `publish_date` date DEFAULT NULL,
   `summary` text DEFAULT NULL,
@@ -67,12 +68,9 @@ CREATE TABLE `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `isbn`, `publish_date`, `summary`, `mockup`, `file`, `author_id`, `book_category_id`, `created_at`, `updated_at`) VALUES
-(1, 'Introduction to Molecular physics\r\n', '4RK2-3927-182H', '2024-02-05', 'A briefly introduction to physics..', NULL, 'physcs.png', 1, 1, NULL, NULL),
-(2, 'The Mystery Of Enoch', 'W537-983H-1982', NULL, NULL, NULL, '../../assets/images/uploads/invitation59.pdf', 1, 4, NULL, NULL),
-(3, 'Biochemistry', '8374-291H-37GW', '0000-00-00', NULL, NULL, '../../assets/images/uploads/invitation413.pdf', 1, 1, NULL, NULL),
-(4, 'Introduction to Chemistry', '25YE-47HH-9392', '0000-00-00', NULL, NULL, '../../assets/images/uploads/invitation107.pdf', 1, 1, NULL, NULL),
-(5, 'The tale of 11 Northern Knights', '2839-9348-7492', '2024-06-03', NULL, NULL, '../../assets/images/uploads/invitation413.pdf', 1, 2, NULL, NULL);
+INSERT INTO `books` (`id`, `title`, `request_id`, `isbn`, `publish_date`, `summary`, `mockup`, `file`, `author_id`, `book_category_id`, `created_at`, `updated_at`) VALUES
+(5, 'The tale of 11 Northern Knights', NULL, '2839-9348-7492', '2024-06-03', NULL, NULL, '../../assets/images/uploads/invitation413.pdf', 1, 2, NULL, NULL),
+(8, 'Bible', 3, '1192-383J-HY73', '2022-05-04', NULL, '../../assets/images/uploads/fly.jpg', '../../assets/images/uploads/invitation413.pdf', 1, 4, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -107,6 +105,7 @@ CREATE TABLE `requests` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `bookname` varchar(100) NOT NULL,
   `author` varchar(100) NOT NULL,
+  `granted` enum('pending','granted') NOT NULL DEFAULT 'pending',
   `user` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -114,8 +113,10 @@ CREATE TABLE `requests` (
 -- Dumping data for table `requests`
 --
 
-INSERT INTO `requests` (`id`, `bookname`, `author`, `user`) VALUES
-(1, 'Order Array', 'Aman Tairo', '4');
+INSERT INTO `requests` (`id`, `bookname`, `author`, `granted`, `user`) VALUES
+(1, 'Order Array', 'Aman Tairo', 'granted', '4'),
+(2, 'Molecular Biology', 'Shedrack K. Shedrack', 'granted', '4'),
+(3, 'The Inigma Of Yang', 'Vacation Time', 'granted', '4');
 
 -- --------------------------------------------------------
 
@@ -163,7 +164,6 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `email_verified_at`, `password`, `role_id`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'tairocruiz', 'admin@domain.com', NULL, '$argon2id$v=19$m=65536,t=4,p=1$YUYzdWNlOGdTNFhubThGWg$GTMOEY7ZQy0/N8EydTwhbQHMd9f5yPnrnw3vMS6jf1U', 1, NULL, NULL, NULL),
 (2, 'Aman Tairo', 'tairo@gmail.com', NULL, '$argon2id$v=19$m=65536,t=4,p=1$YUYzdWNlOGdTNFhubThGWg$GTMOEY7ZQy0/N8EydTwhbQHMd9f5yPnrnw3vMS6jf1U', 2, NULL, NULL, NULL),
-(3, 'DEVID', 'tester@mail.com', NULL, '$argon2id$v=19$m=65536,t=4,p=1$U052WFIzelB6elYuU0xUZA$saVXPgOKexwbygAqLnKs3Rv3mXjpHSqpPWfGkFkM3Kw', 2, NULL, NULL, NULL),
 (4, 'Joshua Gadi', 'joshua@gmail.com', NULL, '$argon2id$v=19$m=65536,t=4,p=1$SEdqMVkueDZuekIuSDlsTQ$RJvGPlSMR7DhUmx5Z9ziu4mjUC2ZvJPH9pYQ3TEnIo0', 2, NULL, NULL, NULL);
 
 --
@@ -184,7 +184,8 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `books_isbn_unique` (`isbn`),
   ADD KEY `books_author_id_foreign` (`author_id`),
-  ADD KEY `books_book_category_id_foreign` (`book_category_id`);
+  ADD KEY `books_book_category_id_foreign` (`book_category_id`),
+  ADD KEY `request_id_contraints` (`request_id`);
 
 --
 -- Indexes for table `book_categories`
@@ -226,7 +227,7 @@ ALTER TABLE `authors`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `book_categories`
@@ -238,7 +239,7 @@ ALTER TABLE `book_categories`
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -250,7 +251,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -267,7 +268,8 @@ ALTER TABLE `authors`
 --
 ALTER TABLE `books`
   ADD CONSTRAINT `books_author_id_foreign` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `books_book_category_id_foreign` FOREIGN KEY (`book_category_id`) REFERENCES `book_categories` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `books_book_category_id_foreign` FOREIGN KEY (`book_category_id`) REFERENCES `book_categories` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `request_id_contraints` FOREIGN KEY (`request_id`) REFERENCES `requests` (`id`);
 
 --
 -- Constraints for table `users`
